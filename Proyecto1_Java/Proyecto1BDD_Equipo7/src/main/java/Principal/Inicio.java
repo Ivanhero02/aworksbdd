afa06a3fe4c23ca5b0d200801909684f503e048c
YungNagy <52930769+YungNagy@users.noreply.github.com>
2022-11-25 20:12:19 -0600

    Se agregaron procedimientos Almacenados y toda la parte de SQL asi como las conexiones
    Las conexiones se crearon correctamente, los procedimientos almacenados fueron creados
    Falta la parte distribuida y corrregir unos metodos de conexion

--- Proyecto1_Java/Proyecto1BDD_Equipo7/src/main/java/Principal/Inicio.java
+++ Proyecto1_Java/Proyecto1BDD_Equipo7/src/main/java/Principal/Inicio.java
@@ -6,13 +6,13 @@ package Principal;
 import SQL.Conexion1;
 import SQL.Conexion2;
 import SQL.Conexion3;
+import java.math.BigDecimal;
 import java.sql.CallableStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.InputMismatchException;
 import java.util.Scanner;
-import javax.swing.JOptionPane;
 
 /**
  *
@@ -22,7 +22,7 @@ public class Inicio{
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args)throws Exception {
     //Comenzamos con un menu para realizar las diferentes consultas
     Scanner sn=new Scanner(System.in);
     boolean salir = false;
@@ -45,8 +45,9 @@ public class Inicio{
             String bases="";
             System.out.println("---Total de Ventas de producto---");
             Conexion1 con1 = new Conexion1();
-            Conexion2 con2 = new Conexion2();
-            Conexion3 con3 = new Conexion3();
+            //Para las conexiones con las demas instancias tomando asi los procedimientos almacenados
+            //Conexion2 con2 = new Conexion2();
+            //Conexion3 con3 = new Conexion3();
             int cat;
             Scanner sn2=new Scanner(System.in);
             try{
@@ -61,8 +62,11 @@ public class Inicio{
                 //Obtenemos los valores 
                 final ResultSet result = pal1.getResultSet();
                 while(result.next()){
-                   System.out.println("Valor1"+result.getRow());
+                   System.out.println(result.getRow()+" Total Ventas:"+result.getString(1)+" Territorio:"+result.getString(2)+" ID Territorio:"+result.getString(3));
                 }
+                result.close();
+                pal1.close();
+                
             }catch(SQLException ex){
                 System.out.println(ex.toString());
             }
@@ -83,21 +87,138 @@ public class Inicio{
             break;
         case 2:
             System.out.println("---Consultar producto mas solicitado para norteamerica---");
+            Conexion1 con1_2 = new Conexion1();
+            try{
+                //Llamamos al procedimiento almacenado
+                CallableStatement pal1 = con1_2.getConexion().prepareCall("{call sp_Ejerciciob}");
+                //Agregamos el valor al procedimiento
+                //Ejecutamos la consulta al procedimiento
+                pal1.execute();
+                //Obtenemos los valores 
+                final ResultSet result = pal1.getResultSet();
+                while(result.next()){
+                   System.out.println(result.getRow()+" Producto:"+result.getString(1)+" Solicitudes:"+result.getString(2)+" Region:"+result.getString(3));
+                }
+                
+            }catch(SQLException ex){
+                System.out.println(ex.toString());
+            }
+            
             break;
         case 3:
             System.out.println("---Actualizar Stock por categoria un 5%---");
+            int cat2,ID;
+            Scanner sn3=new Scanner(System.in);
+            Conexion1 con1_3 = new Conexion1();
+            try{
+                System.out.println("\nIntroduzca la Categoria del producto:");
+                cat2=sn3.nextInt();
+                System.out.println("\nIntroduzca la locacion ID:");
+                ID=sn3.nextInt();
+                //Llamamos al procedimiento almacenado
+                CallableStatement pal1 = con1_3.getConexion().prepareCall("{call sp_Ejercicioc(?,?)}");
+                //Agregamos el valor al procedimiento
+                pal1.setInt(1,cat2);
+                pal1.setInt(2, ID);
+                //Ejecutamos la consulta al procedimiento
+                pal1.execute();
+                //Obtenemos los valores 
+                System.out.println("Stock Actualizado");
+            }catch(SQLException ex){
+                System.out.println(ex.toString());
+            }
             break;
         case 4:
             System.out.println("---Determinacion de clientes en otras regiones---");
+            Conexion1 con1_4 = new Conexion1();
+            try{
+                //Llamamos al procedimiento almacenado
+                CallableStatement pal1 = con1_4.getConexion().prepareCall("{call sp_Ejerciciod}");
+                //Agregamos el valor al procedimiento
+                //Ejecutamos la consulta al procedimiento
+                pal1.execute();
+                //Obtenemos los valores 
+                final ResultSet result = pal1.getResultSet();
+                while(result.next()){
+                   System.out.println(result.getRow()+" ID: "+result.getString(1)+" TeritorioPedido:"+result.getString(2)+" TerritorioMandado:"+result.getString(3));
+                }
+            }catch(SQLException ex){
+                System.out.println(ex.toString());
+            }
             break;
         case 5:
             System.out.println("---Actualizar cantidad de productos por orden---");
+            int ID2,PID,cant;
+            Scanner sn4=new Scanner(System.in);
+            Conexion1 con1_5 = new Conexion1();
+            try{
+                System.out.println("\nIntroduzca el ID:");
+                ID2=sn4.nextInt();
+                System.out.println("\nIntroduzca el ID del producto:");
+                PID=sn4.nextInt();
+                System.out.println("\nIntroduzca la cantidad:");
+                cant=sn4.nextInt();
+                //Llamamos al procedimiento almacenado
+                CallableStatement pal1 = con1_5.getConexion().prepareCall("{call sp_Ejercicioe(?,?,?)}");
+                //Agregamos el valor al procedimiento
+                pal1.setInt(1,ID2);
+                pal1.setInt(2, PID);
+                pal1.setInt(3,cant);
+                //Ejecutamos la consulta al procedimiento
+                pal1.execute();
+                //Obtenemos los valores 
+               System.out.println("Cantidad Actualizada");
+            }catch(SQLException ex){
+                System.out.println(ex.toString());
+            }
             break;
         case 6:
             System.out.println("---Actualizar Metodo de envio de una orden---");
+            int ID3,dir;
+            Scanner sn5=new Scanner(System.in);
+            Conexion1 con1_6 = new Conexion1();
+            try{
+                System.out.println("\nIntroduzca el ID:");
+                ID3=sn5.nextInt();
+                System.out.println("\nIntroduzca el ID del metodo de envio nuevo:");
+                dir=sn5.nextInt();
+                //Llamamos al procedimiento almacenado
+                CallableStatement pal1 = con1_6.getConexion().prepareCall("{call sp_Ejercicioe(?,?)}");
+                //Agregamos el valor al procedimiento
+                pal1.setInt(1,ID3);
+                pal1.setInt(2, dir);
+                //Ejecutamos la consulta al procedimiento
+                pal1.execute();
+                //Obtenemos los valores 
+                 System.out.println("Metodo Actualizado");
+            }catch(SQLException ex){
+                System.out.println(ex.toString());
+            }
             break;
         case 7:
             System.out.println("---Actualizar correo electronico de cliente---");
+            int IDP;
+            String email;
+            Scanner sn6=new Scanner(System.in);
+            Conexion1 con1_7 = new Conexion1();
+            try{
+                System.out.println("\nIntroduzca el ID de la persona:");
+                IDP=sn6.nextInt();
+                System.out.println("\nIntroduzca el nuevo email:");
+                email=sn6.next();
+                //Llamamos al procedimiento almacenado
+                CallableStatement pal1 = con1_7.getConexion().prepareCall("{call sp_Ejercicioe(?,?)}");
+                //Agregamos el valor al procedimiento
+                pal1.setInt(1,IDP);
+                pal1.setString(2, email);
+                //Ejecutamos la consulta al procedimiento
+                pal1.execute();
+                //Obtenemos los valores 
+                
+                System.out.println("Email Actualizado");
+            }catch(SQLException ex){
+                System.out.println(ex.toString());
+            }
             break;
         case 8:
             salir=true;
